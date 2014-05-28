f7a0358743d99bf85568ccfca41b38037f4c7514
Yuri van Geffen <yuri@yurivangeffen.com>
2014-05-28 13:49:47 +0200

    Initial SubDebug.py, STEPs immediately on incomming connections.

---
+++ subdebug.py
@@ -0,0 +1,62 @@
+import sublime, sublime_plugin
+
+import queue
+import asyncore
+import socket
+import threading
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 8172
+BUFFER_SIZE = 1024
+
+class SubDebugHandler(asyncore.dispatcher):
+	def __init__(self, socket):
+		asyncore.dispatcher.__init__(self, socket)
+		msg_queue.put(b"STEP\n")
+
+	def handle_read(self):
+		data = self.recv(BUFFER_SIZE)
+		if data:
+			print("Received: ", data)
+
+	def handle_write(self):
+		if not msg_queue.empty():
+			msg = msg_queue.get()
+			print("Sending: ", msg)
+			self.send(msg)
+
+	def handle_error(self):
+		raise
+
+
+class SubDebugServer(asyncore.dispatcher):
+
+	def __init__(self, host, port):
+		asyncore.dispatcher.__init__(self)
+		self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.set_reuse_addr()
+		self.bind((host, port))
+		self.listen(1)
+		print("Started listening on: ", host, ":", port)
+
+
+	def handle_accept(self):
+		pair = self.accept()
+		if pair is not None:
+			(conn_sock, client_address) = pair
+			print("Incoming connection: ", client_address)
+			SubDebugHandler(conn_sock)
+			#conn_sock.send(b"STEP\n")
+
+	def handle_close(self):
+		print("Closing server.")
+		self.close()
+
+	def handle_error(self):
+		self.close()
+
+msg_queue = queue.Queue()
+server = SubDebugServer(TCP_IP, TCP_PORT)
+asyncore.loop()
+
+		
